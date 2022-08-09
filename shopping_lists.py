12ca31c2c45e2ca199b6096fc2919779cc6ff002
phucnguyen02 <ndhp02@gmail.com>
2022-08-08 21:10:17 -0700

    Removed some misc

--- shopping_lists.py
+++ shopping_lists.py
@@ -18,14 +18,5 @@ c = conn.cursor()
 #        name text
 #        )""")
 
-
-# insert_list(list1)
-# insert_list(list2)
-#rename_list(list1, "Shopping List 3")
-# c.execute("INSERT INTO shopping_lists (name) VALUES('Shopping List 1')")
-# conn.commit()
-# c.execute("INSERT INTO shopping_lists (name) VALUES('Shopping List 2')")
-# conn.commit()
-
 conn.commit()
 conn.close()
